1fca23eadf189478604980510f101a72c8de0b51
ColmofWar <joshuaallendimarco@gmail.com>
2025-09-16 21:45:10 -0400

    Navbar and css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,20 +4,42 @@ import "../css/NavBar.css";
 
 function NavBar() {
     return (
-        <nav className="NavBar">
-            <Link to="/">
-                Home
-            </Link>
-            <Link to="/shop">
-                Shop
-            </Link>
-            <Link to="/profile">
-                Profile
-            </Link>
-            <Link to="/login">
-                Login
-            </Link>
-        </nav>
+    <nav className="navbar">
+        <div className="navbar__inner">
+            <a href="/" className="navbar__brand">
+                <div className="navbar__logo">S</div>
+                <div className="navbar__title">Skinn Savvy</div>
+            </a>
+
+            <div className="navbar__links">
+                <a className="navlink navlink--active" >
+                <Link to="/">
+                    Home
+                </Link>
+                </a>
+                <a className="navlink">
+                <Link to="/shop">
+                    Shop
+                </Link>
+                </a>
+                <a className="navlink">
+                <Link to="/profile">
+                    Profile
+                </Link>
+                </a>
+            </div>
+
+            <div style={{display: 'flex', alignItems: 'center', gap: '0.75rem'}}>
+                <a className="navbar__cta">
+                <Link to="/login">
+                        Login
+                </Link>
+                </a>
+                <button className="navbar__menu-toggle" aria-label="Open menu">☰</button>
+            </div>
+        </div>
+    </nav>
     )
 }
 export default NavBar;
+
